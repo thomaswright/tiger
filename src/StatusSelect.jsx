c7d95481fe6f70a7d771d1b92fa37a5aad18707a
Thomas Wright <thomaswright1@gmail.com>
2024-10-18 13:21:04 +0900

    wip

--- src/StatusSelect.jsx
+++ src/StatusSelect.jsx
@@ -15,10 +15,24 @@ const Dropdown = ({ status, setStatus, removeTodo }) => {
     return (
       <DropdownMenu.Item
         key={s}
+        style={{
+          // borderColor: statusColor(s),
+          // backgroundColor:
+          //   status === s
+          //     ? statusColor(s)
+          //     : `oklch(from ${statusColor(s)} 0.95 calc(c / 3) h)`,
+          // color: status === s ? "var(--t0)" : statusColor(s),
+          color: "var(--t0)",
+          backgroundColor:
+            status === s
+              ? statusColor(s)
+              : `oklch(from ${statusColor(s)} 0.85 calc(c / 1.5) h)`,
+        }}
         className={[
-          "font-black",
+          status === s ? "" : "",
+          ` rounded font-bold flex flex-row items-center justify-center 
+          relative h-6 w-6 select-none `,
           isArchiveStatus(s) ? "bg-[var(--t2)]" : "",
-          status === s ? "DropdownMenuItemSelected" : "DropdownMenuItem",
         ].join(" ")}
         onSelect={(_) =>
           s === "" ? {} : s === "Trash" ? removeTodo() : setStatus(s)
@@ -39,7 +53,12 @@ const Dropdown = ({ status, setStatus, removeTodo }) => {
       <DropdownMenu.Trigger asChild>
         <button
           style={{
-            backgroundColor: statusColor(status),
+            // backgroundColor: statusColor(status),
+
+            color: statusColor(status),
+            backgroundColor: `oklch(from ${statusColor(
+              status
+            )} 0.95 calc(c / 3) h)`,
           }}
           className=" flex flex-row items-center text-sm justify-center w-5 h-5 font-bold rounded text-[var(--t0)]"
           aria-label="Customise options"
@@ -52,28 +71,38 @@ const Dropdown = ({ status, setStatus, removeTodo }) => {
         <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
           <div>
             <div className="h-6 font-bold flex flex-row items-center justify-center">
-              <div className=" w-6 flex flex-row items-center justify-center">
+              {/* <div className=" w-6 flex flex-row items-center justify-center">
                 {statusStringShort(hoverStatus)}
-              </div>
+              </div> */}
               <div>{statusString(hoverStatus)}</div>
             </div>
-            <div className="grid grid-cols-4">
-              {item("TodoUrgent")}
-              {item("TodoHigh")}
-              {item("TodoMedium")}
-              {item("TodoLow")}
-              {item("LaterUnsorted")}
-              {item("LaterWill")}
-              {item("LaterMaybe")}
-              {item("LaterUnlikely")}
-              {item("ResolveDone")}
-              {item("ResolveReject")}
-              {item("ResolveNoNeed")}
-              {item("")}
-              {item("ArchiveDone")}
-              {item("ArchiveReject")}
-              {item("ArchiveNoNeed")}
-              {item("Trash")}
+            <div className="flex flex-col gap-2 items-center pb-2 pt-1">
+              <div className="flex flex-row gap-1">
+                {item("TodoUrgent")}
+                {item("TodoHigh")}
+                {item("TodoMedium")}
+                {item("TodoLow")}
+              </div>
+              <div className="flex flex-row gap-1">
+                {item("LaterUnsorted")}
+                {item("LaterWill")}
+                {item("LaterMaybe")}
+                {item("LaterUnlikely")}
+              </div>
+
+              <div className="flex flex-row gap-1">
+                {item("ResolveDone")}
+                {item("ResolveReject")}
+                {item("ResolveNoNeed")}
+                {item("")}
+              </div>
+
+              <div className="flex flex-row gap-1">
+                {item("ArchiveDone")}
+                {item("ArchiveReject")}
+                {item("ArchiveNoNeed")}
+                {item("Trash")}
+              </div>
             </div>
           </div>
 
