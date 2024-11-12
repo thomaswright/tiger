a08c7720d27ee42f7fcbe706a4400cd3f2b5747c
Thomas Wright <thomaswright1@gmail.com>
2024-11-11 20:32:08 -0600

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,9 +9,4 @@ export default defineConfig({
       include: ["**/*.res.mjs"],
     }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
