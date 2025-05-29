6a0027a6d3122204993fb5f32e6d23cbbc7e9491
Thomas Wright <thomaswright1@gmail.com>
2025-05-29 12:09:01 +0900

    customize login

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
   setTodoModesShown,
 } from "./utils/SupaLegend.ts";
 import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { ThemeSupa, minimal } from "@supabase/auth-ui-shared";
 import Entry from "./Entry.res.mjs";
 import useSessionStorage from "./useSessionStorage.js";
 import { jwtDecode } from "jwt-decode";
@@ -65,9 +65,38 @@ function App() {
   if (!session) {
     return (
       <div className="max-w-lg p-6">
+        <div className="flex flex-row gap-2 items-center justify-center">
+          <img src={logoUrl} width={"24"} className="" />
+          <div className="font-bold text-2xl"> {"Tiger Todo"} </div>
+        </div>
         <Auth
           supabaseClient={supabase}
-          appearance={{ theme: ThemeSupa }}
+          appearance={{
+            theme: ThemeSupa,
+            variables: {
+              default: {
+                colors: {
+                  brand: "#ff9400",
+                  brandAccent: "#FF8000",
+                },
+                radii: {
+                  borderRadiusButton: "8px",
+                  buttonBorderRadius: "8px",
+                  inputBorderRadius: "8px",
+                },
+                fontSizes: {
+                  baseBodySize: "13px",
+                  baseInputSize: "14px",
+                  baseLabelSize: "14px",
+                  baseButtonSize: "14px",
+                },
+                space: {
+                  buttonPadding: "6px 10px",
+                  inputPadding: "6px 10px",
+                },
+              },
+            },
+          }}
           providers={[]}
         />
       </div>
